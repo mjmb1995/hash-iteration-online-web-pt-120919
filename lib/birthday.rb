993f6c126f179b97c750dab3ea5a537b664cdef8
Mark Bihag <markbihag330@gmail.com>
2019-12-22 22:44:18 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,6 +15,8 @@ birthday_kids.each do |kids_name, age|
 
   if age < 13
      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+   else
+ puts "You are too old for this."
   end
 
 end
